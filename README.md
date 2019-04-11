b4dd6ec986facf712a9bfa63202385fe89eed654
Aaron Pham <Aaronpham0103@gmail.com>
2019-04-11 11:55:15 -0700

    uses sqlite3 as a final decision for database

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # tob
 
 1. Processing Database for the bot
-  - using Reddit comments from [pushshift](https://files.pushshift.io/reddit/comments/)
+  - using Reddit comments from [pushshift.io](https://files.pushshift.io/reddit/comments/)
 
 ## Woking in progress
 - [ ] Processing RAW JSON file
--- database.py
+++ database.py
@@ -1,15 +1,34 @@
 import sqlite3
 import json
-from datetime import datetime
+import datetime
 
-timeframe = "2005-12"
-sql_trans = []
+tf = '2005-12'
+trans = []
 
-connection = sqlite3.connect('{}.db'.format(timeframe))
+connection = sqlite3.connect('{}.db'.format(tf))
+
 c = connection.cursor()
 
+
 def create_table():
-    c.execute("CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
+    c.execute('''CREATE TABLE IF NOT EXIST parent_reply(
+        parent_id TEXT PRIMARY KEY,
+        comment_id TEXT UNIQUE,
+        parent TEXT,
+        comment TEXT,
+        subreddit TEXT,
+        unix INT,
+        score INT)'''
+              )
+
 
 if __name__ == '__main__':
     create_table()
+    row_counter = 0
+    paired_rows = 0
+
+with open('/home/aazasdass/Documents/Coding/tob/RC/RC_{}'.format(tf.split('-')[0], tf), buffering=1000) as f:
+    for r in f:
+        row_counter += 1
+        row = json.loads(row)
+        parent_id = row['parent_id']
