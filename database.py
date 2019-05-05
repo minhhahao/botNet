f6d224dbf461b02b5a37585cc79b0d8322b48c39
Aaron Pham <aaronpham0103@gmail.com>
2019-05-05 15:04:36 -0700

    trying to fix how to parse data into pd

--- database.py
+++ database.py
@@ -1,74 +1,66 @@
 '''
     Processing data from http://files.pushshift.io/reddit/comments/
-    Database of choice: Pandas / MongoDB
+    Database of choice: Pandas
 '''
+from datetime import datetime
 import pandas as pd
 import os
-from datetime import datetime
-from pymongo import MongoClient
 
 
-class handler():
-    # path
-    output_dir = ''
+class dataHandler():
 
-    # important column
-    cols_to_keep = ["parent_id", "created_utc",
-                    "subreddit_id", "score", "body", "name"]
+    global db
+    # timeframe for RC = [201001, 201010, 201501], using 201001
+    timeframe = 0
+    # Directory path
+    RC_dir = ' '
+    output_dir = ' '
+    tmp_dir = os.path.join(os.getcwd(), 'database',
+                           'output', 'temp', 'chunk.json')
+    # important columns
+    cols_to_keep = []
 
-    # timeframe for RC
-    # timeframe = ['2010-01', '2010-10', '2015-01']
-    timeframe = '2010-01'
+    def __init__(self, RC, output, important_column, tf):
+        if os.path.exists(RC) and os.path.exists(output):
+            self.RC_dir = RC
+            self.output_dir = output
+            self.cols_to_keep = important_column
+            self.timeframe = tf
+        else:
+            print("Invalid directory")
 
-    def __init__(self):
-        self.output_dir = os.path.join(os.getcwd(), 'database', 'output')
+    def checkSize(file):
+        size = os.path.getsize(file)
+        return size
 
-    def read_mongo(database, collection, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
-        '''
-        Usage: Read query from MongoDB then put into Pandas DataFrame
-        '''
-        # Connect to MongoDB
-        conn = MongoClient(host=host, port=port, username=username,
-                           password=password, authSource="Admin")
-        db = conn['database']
-
-        # Make query to MongoDB
-        cur = db.collection.find(query)
-
-        # Expand the cursor and construct pd DataFrame
-        df = pd.DataFrame(list(cur))
-
-        # Delete _id that exists for each document in MongoDB
-        if no_id:
-            del df['id']
-
-        return df
-
-    def process_data(self):
-        db = self.read_mongo()
-        column = self.cols_to_keep
-        # Filter out necessary data array
-        df = db.drop(db.columns.difference(column), axis=1)
+    def process_data(file=None, column=[]):
+        print("Begin Processing @ " + str(datetime.now()))
+        df = pd.read_json(file, orient='columns')
+        db = df.drop(df.columns.difference(column), axis=1)
         # Filter array for comment
-        df = df[~df['body'].isin(['[deleted]', '[removed]'])]
+        db = db[~db['body'].isin(['[deleted]', '[removed]'])]
         # Drop all the comment that have a score < 0
-        df.drop(df[df['score'] < 3].index, inplace=True)
+        db.drop(db[db['score'] < 3].index, inplace=True)
         # Remove special character from comments
-        df = df.replace('\n', '', regex=True)
-        print("Finished creating DB @ " + str(datetime.now()))
-        return df
+        db = db.replace('\n', '', regex=True)
+        print("Process finished @ " + str(datetime.now()))
+        return db
 
-    def training_data(self, output_file, comments=['body']):
-        database = self.read_mongo()
-        bd = database[comments]
-        if not os.path.exists(output_file):
-            print("Writing output file @ " + str(datetime.now()))
-            with open(output_file, 'a', encoding="utf-8") as output:
-                bd.to_csv(output, sep=' ', index=False, header=False)
+    def output_data(file=None, comment=' '):
+        with open(file, 'w+', encoding='utf-8') as output:
+            print("Start writing outputs @ " + str(datetime.now()))
+            database = db[comment]
+            out = database.to_csv(output, sep='', index=False, header=False)
+        print("Finished @ " + str(datetime.now()))
+        return out
 
     if __name__ == '__main__':
-        read_mongo('RC', 2010 - 10, {}, host='localhost', port= 27017,
-                   username='root', password='toor', no_id=True)
-        process_data()
-        training_data(os.path.join(
-            os.getcwd(), 'database', 'output', 'db.txt'))
+        tf = 201501
+        RC = str(os.path.join(os.getcwd(), 'database', 'RC',
+                              "RC_{}.json".format(tf)))
+        output = str(os.path.join(os.getcwd(), 'database', 'output',
+                                  'dataset.txt'))
+        important_col = ["parent_id", "created_utc",
+                         "subreddit_id", "score", "body", "name"]
+        process_data(file=RC, column=important_col)
+        output_data(file=output, comment='body')
