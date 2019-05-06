bcda9731f06f28bf279a869496df58c74adc8795
Aaron Pham <aaronpham0103@gmail.com>
2019-05-06 08:33:28 -0700

    updates with parsing big data into temp file. TODO: Working on bash file for Windows compatibility

--- data.py
+++ data.py
@@ -5,37 +5,40 @@
 from datetime import datetime
 import pandas as pd
 import os
+import glob
 
 
 class dataHandler():
 
     global db
-    # timeframe for RC = [201001, 201010, 201501], using 201001
+    # timeframe for RC = [201001, 201010, 201501, 201601], using 201001
     timeframe = 0
     # Directory path
     RC_dir = ' '
     output_dir = ' '
-    tmp_dir = os.path.join(os.getcwd(), 'database',
-                           'output', 'temp', 'chunk.json')
+    tmp_dir = ' '
     # important columns
     cols_to_keep = []
 
-    def __init__(self, RC, output, important_column, tf):
+    def __init__(self, RC, output, tmp, important_column, tf):
         if os.path.exists(RC) and os.path.exists(output):
             self.RC_dir = RC
             self.output_dir = output
+            self.tmp_dir = tmp
             self.cols_to_keep = important_column
             self.timeframe = tf
         else:
             print("Invalid directory")
 
-    def checkSize(file):
-        size = os.path.getsize(file)
-        return size
-
-    def process_data(file=None, column=[]):
+    def process_data(file=None, column=[], tmp_dir=None):
         print("Begin Processing @ " + str(datetime.now()))
-        df = pd.read_json(file, orient='columns')
+        if os.path.getsize(file) > 16e9:
+            os.system('sh database/process_big_data.sh')
+            for tmp_file in glob(tmp_dir):
+                df = pd.read_json(tmp_file, orient='column')
+                os.remove(tmp_file)
+        else:
+            df = pd.read_json(file, orient='columns')
         db = df.drop(df.columns.difference(column), axis=1)
         # Filter array for comment
         db = db[~db['body'].isin(['[deleted]', '[removed]'])]
@@ -55,12 +58,14 @@ class dataHandler():
         return out
 
     if __name__ == '__main__':
-        tf = 201501
+        tf = 201601
         RC = str(os.path.join(os.getcwd(), 'database', 'RC',
                               "RC_{}.json".format(tf)))
         output = str(os.path.join(os.getcwd(), 'database', 'output',
                                   'dataset.txt'))
         important_col = ["parent_id", "created_utc",
                          "subreddit_id", "score", "body", "name"]
-        process_data(file=RC, column=important_col)
+        tmp = str(os.path.join(os.getcwd(), 'database', 'temp'))
+
+        process_data(file=RC, column=important_col, tmp_dir=tmp)
         output_data(file=output, comment='body')
