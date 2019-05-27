ff9906ba8ad7aa67746d170ca16aab5707246ccc
Aaron Pham <aaronpham0103@gmail.com>
2019-05-27 10:55:14 -0700

    updates training params

--- config.py
+++ config.py
@@ -2,21 +2,20 @@ import os
 
 # Init path
 DATA_PATH = os.path.join('data', 'cornell')
-CKPT_PATH = os.path.join('data', 'save', 'model-{epoch:03d}.ckpt')
-CKPT_DIR = os.path.dirname(CKPT_PATH)
 LINES_FILE = 'movie_lines.txt'
 CONVERSATIONS_FILE = 'movie_conversations.txt'
-
+VOCAB_FILE = 'vocab'
 # Data processing parameters
-MAX_SAMPLES = 25000
+MAX_SAMPLES = 50000
 MAX_LENGTH = 40
 BATCH_SIZE = 64
 BUFFER_SIZE = 20000
 
 # Hyper-parameters
-NUM_LAYERS = 3
-D_MODEL = 512
+NUM_LAYERS = 2
+D_MODEL = 256
 NUM_HEADS = 8
-UNITS = 1024
-DROPOUT = 0.3
-EPOCHS = 20 # set 20 for first time training
+UNITS = 512
+DROPOUT = 0.1
+EPOCHS = 20
+# 20 is optimum considering learning rate decay
